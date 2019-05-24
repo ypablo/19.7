37f9245d7a0c464bbc5f86c68098a08f405b6907
ypablo <yfedora@gmail.com>
2019-05-24 18:28:12 +0100

    React timer

--- script.js
+++ script.js
@@ -1,94 +1,10 @@
 
-
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			txt: ""
-		}
-		this.handleClick = () => {
-			const number = Math.floor(Math.random() * 10)
-			this.setState({
-				txt: this.state.txt + number
-			})
-		}
-	}
-
-	render() {
-		return (
-			<div>
-				<button className="bt" onClick={this.handleClick.bind(this)}>{this.props.btnTitle}</button>
-				<h1>{this.state.txt}</h1>
-			</div>
-		)
-	}
-}
-
-ReactDOM.render(<App btnTitle="add digit" />, document.getElementById("app1"))
-
-//-------------------------------------------------//
-
-class Counter extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			count: 0,
-			result: 0
-		}
-	}
-
-	handleMathClick(type, number) {
-		if (type === "sub") {
-			this.setState(prevState => ({
-				count: prevState.count + 1,
-				result: prevState.result - number
-			}))
-		} else if (type === "reset") {
-			this.setState(prevState => ({
-				count: prevState.count + 1,
-				result: 0
-			}))
-		} else if (type === "add") {
-			this.setState(prevState => ({
-				count: prevState.count + 1,
-				result: prevState.result + number
-			}))
-		}
-	}
-
-	render() {
-		return (
-			<div>
-				<button onClick={this.handleMathClick.bind(this, "sub", 1)}>-1</button>
-				<button onClick={() => this.handleMathClick("reset")}>RESET</button>
-				<button onClick={() => this.handleMathClick("add", 1)}>+1</button>
-				<h1>Clicks number: {this.state.count}</h1>
-				<h1>Result: {this.state.result}</h1>
-			</div>
-		)
-	}
-
-}
-
-ReactDOM.render(<Counter />, document.getElementById("app2"))
-
-
-//-------------------------------------------------//
-
-function pad0(value) {
-	let result = value.toString();
-	if (result.length < 2) {
-		result = '0' + result;
-	}
-	return result;
-}
-
 class Stopwatch extends React.Component {
 
-	constructor(display) {
-		super(display);
+	constructor(props) {
+		super(props);
 		this.running = false;
-		this.display = display;
+		this.display = props.display;
 		this.reset();
 		this.print(this.times);
 
@@ -141,48 +57,19 @@ class Stopwatch extends React.Component {
 		this.running = false;
 		clearInterval(this.watch);
 	}
-	//}
 
-	/*
-	pad0() {
-		return "AAA"
-	}
-	
-	
-	pad0(value) {
-		let result = value.toString();
-		if (result.length < 2) {
-			result = '0' + result;
-		}
-		return result;
-	}
-		
-		
-	/*
-	const stopwatch = new Stopwatch(
-	document.querySelector('.stopwatch'));
-		
-	let startButton = document.getElementById('start');
-	startButton.addEventListener('click', () => stopwatch.start());
-		
-	let stopButton = document.getElementById('stop');
-	stopButton.addEventListener('click', () => stopwatch.stop());
-		
-	let resetButton = document.getElementById('reset');
-	resetButton.addEventListener('click', () => stopwatch.reset());
-		
-		
-	*/
 
 	render() {
 		return (
 			<div>
 				<nav className="controls">
-					<a href="#" className="button" id="start" onClick={this.start}>Start</a>
-					<a href="#" className="button" id="stop" onClick={this.stop}>Stop</a>
+					<a href="#" className="button" id="start" onClick={this.start.bind(this)}>Start</a>
+					<a href="#" className="button" id="stop" onClick={this.stop.bind(this)}>Stop</a>
 				</nav>
 				<div className="stopwatch">{this.display}</div>
-				<ul className="results"></ul>
+				<ul className="results">
+					<a href="#" className="button" id="reset" onClick={this.reset.bind(this)}>Reset</a>
+				</ul>
 			</div>
 		)
 	}
@@ -193,82 +80,4 @@ ReactDOM.render(<Stopwatch />, document.getElementById('app3'));
 
 
 
-
-
-/*
-class Stopwatch {
-    constructor(display) {
-        this.running = false;
-        this.display = display;
-        this.reset();
-        this.print(this.times);
-    }
-
-	reset() {
-        this.times = {
-            minutes: 0,
-            seconds: 0,
-            miliseconds: 0
-        };
-		this.print();
-    }
-
-	print() {
-        this.display.innerText = this.format(this.times);
-	}
-
-	format(times) {
-        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-	}
-
-	start() {
-    if (!this.running) {
-        this.running = true;
-        this.watch = setInterval(() => this.step(), 10);
-		}
-	}
-
-	step() {
-		if (!this.running) return;
-		this.calculate();
-		this.print();
-	}
-	calculate() {
-		this.times.miliseconds += 1;
-		if (this.times.miliseconds >= 100) {
-			this.times.seconds += 1;
-			this.times.miliseconds = 0;
-		}
-		if (this.times.seconds >= 60) {
-			this.times.minutes += 1;
-			this.times.seconds = 0;
-		}
-	}
-
-	stop() {
-		this.running = false;
-		clearInterval(this.watch);
-	}
-}
-
-function pad0(value) {
-    let result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
-    }
-    return result;
-}
-
-const stopwatch = new Stopwatch(
-document.querySelector('.stopwatch'));
-
-let startButton = document.getElementById('start');
-startButton.addEventListener('click', () => stopwatch.start());
-
-let stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', () => stopwatch.stop());
-
-let resetButton = document.getElementById('reset');
-resetButton.addEventListener('click', () => stopwatch.reset());
-*/
 
